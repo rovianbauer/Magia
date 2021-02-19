e756ff8a06c7504f873c06fec2e747425a1afb95
Rovian Bauer <rovianbauer@hotmail.com>
2021-02-19 00:36:03 -0300

    Implementado regra de validação para agendamento

--- Magia/Magia.Application/AgendamentoContext/CommandHandlers/NovoAgendamentoCommandHandler.cs
+++ Magia/Magia.Application/AgendamentoContext/CommandHandlers/NovoAgendamentoCommandHandler.cs
@@ -1,7 +1,11 @@
 ﻿using Magia.Application.Agendamento.Commands;
+using Magia.Domain.AgendamentoContext.Entities;
+using Magia.Domain.AgendamentoContext.Interfaces;
 using Magia.Domain.Core.Commands;
 using Magia.Domain.Core.Notification;
 using MediatR;
+using System.Globalization;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -10,17 +14,57 @@ namespace Magia.Application.Agendamento.CommandHandlers
     public class NovoAgendamentoCommandHandler :
         BaseCommandHandler, IRequestHandler<NovoAgendamentoCommand, bool>
     {
+        private readonly ISalaRepository _salaRepository;
+        private readonly IAgendamentoRepository _agendamentoRepository;
+
         public NovoAgendamentoCommandHandler(IMediator mediator,
-            IDomainNotificationHandler notification) : base(mediator, notification)
+            IDomainNotificationHandler notification,
+            ISalaRepository salaRepository,
+            IAgendamentoRepository agendamentoRepository)
+            : base(mediator, notification)
         {
+            _salaRepository = salaRepository;
+            _agendamentoRepository = agendamentoRepository;
         }
 
         public async Task<bool> Handle(NovoAgendamentoCommand request, CancellationToken cancellationToken)
         {
-            var x = 1;
+            var salaAgendamento
+                = await _salaRepository.ObtemPorIdAsync(request.SalaId);
 
+            if (salaAgendamento == null || salaAgendamento.Deletado)
+            {
+                AddNotification("Sala não encontrada");
+                return false;
+            }
 
-            return true;
+            var novoAgendamento = new AgendamentoEntity(salaAgendamento,
+                request.Titulo,
+                request.DataHoraInicio,
+                request.DataHoraFim);
+
+            if (novoAgendamento.IsValid())
+            {
+                var agendamentoEmConflito =
+                    (await _agendamentoRepository.BuscarAsync(x => x.Sala.Id == request.SalaId
+                                                                &&
+                                                                ((x.DataHoraInicio <= request.DataHoraInicio && x.DataHoraFim >= request.DataHoraInicio)
+                                                                 || (x.DataHoraInicio <= request.DataHoraFim && x.DataHoraFim >= request.DataHoraFim)
+                                                                 || (x.DataHoraInicio >= request.DataHoraInicio && x.DataHoraFim <= request.DataHoraFim))
+                                                              ));
+                var cultura = new CultureInfo("pt-BR");
+
+                if (!agendamentoEmConflito.Any())
+                    await _agendamentoRepository.InsereAsync(novoAgendamento);
+                else
+                    AddNotification($"A Sala ({salaAgendamento.Descricao}) já possui um agendamento com " +
+                                    $"inicio {agendamentoEmConflito.First().DataHoraInicio.ToString("F", cultura)} " +
+                                    $"e fim {agendamentoEmConflito.First().DataHoraFim.ToString("F", cultura)}");
+            }
+
+            AddNotifications(novoAgendamento);
+
+            return !Notification.HasNotifications;
         }
     }
 }
